1908e059c646c3d790690fa08c08e960cd979fb2
Bastian Petric <beartheylatino@gmail.com>
2025-08-11 16:22:24 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(228,194,59);
+}
+
+function draw() {
+  d = random(55,70);
+  fill(255,random(55,200),70);
+  ellipse(mouseX,mouseY,d,d);
+}
