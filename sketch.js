a60033c2d2d7444d402c0cbe2d179dad07316859
Bastian Petric <beartheylatino@gmail.com>
2025-08-11 17:34:54 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,10 +1,17 @@
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  background(228,194,59);
+  createCanvas(windowWidth, windowHeight).position(0,0).style('z-index','-1');
+  background(228,194,59)
+  colorMode (HSB);
 }
 
 function draw() {
-  d = random(55,70);
-  fill(255,random(55,200),70);
+  d = random(45,70);
+  fill(random(20,10,0),random(0,100),100);
   ellipse(mouseX,mouseY,d,d);
+  fill(random(0,70,95),random(20,80,60),100);
+  square(mouseX,mouseY, 35);
+}
+  function keyPressed() {
+  if (key === 'p') {
+    save('dibujo.jpg');}
 }
