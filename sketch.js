2ca8a2d80fc3e3a159a5a7d08c93b7673bac9d8b
Bastian Petric <beartheylatino@gmail.com>
2025-08-11 16:57:26 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,4 +7,9 @@ function draw() {
   d = random(55,70);
   fill(255,random(55,200),70);
   ellipse(mouseX,mouseY,d,d);
+
+  function keyPressed() {
+  if (key === 'p') {
+    save('dibujo.jpg');
+  }
 }
